d20b202e40357a12e808951fa07cc2be5ca44061
Alex Hagiopol <alexhagiopol@gmail.com>
2018-09-17 11:12:35 -0700

    add user interface. still numerically unstable.

--- gmm_segmentation.py
+++ gmm_segmentation.py
@@ -9,14 +9,9 @@ mpl.use('TkAgg')  # hack around bug in matplotlib. see https://stackoverflow.com
 from matplotlib import pyplot as plt
 import numpy as np
 import scipy as sp
+import sys
+import os
 from scipy.stats import norm
-
-
-# tuning parameters
-FILEPATH = "dist_1.png"
-COMPONENTS = 2
-ITERATIONS = 10
-INIT_VARIANCE = np.float32(1/255)
 
 
 def show_image(location, title, img, width=15, height=3, open_new_window=True, vmin=-5000.0, vmax=5000.0, cmap='gray', fontsize=10):
@@ -46,41 +41,55 @@ def show_image(location, title, img, width=15, height=3, open_new_window=True, v
         plt.close()
 
 
-def main():
-    image = np.float32(cv2.imread(FILEPATH))
-    image = image / np.float32(255)
+def usage():
+    print("Incorrect usage. Example:\npython gmm_segmentation.py image_filepath num_components num_iterations")
+
+
+def execute_segmentation(filepath, components, iterations, init_variance=np.float64(1/255)):
+    """
+    :param filepath: path to input grayscale image
+    :param components: number of gaussian models to fit to the image
+    :param iterations: number of iterations of the expectation maximization algorithm
+    :param init_variance: initial variance for each model
+    :return:
+    """
+
+    image = np.float64(cv2.imread(filepath))
+    image = image / np.float64(255)  # normalize image
     rows, cols, chans = image.shape
-    print("Starting Gaussian Mixture Model segmentation for", FILEPATH)
+    print("Starting Gaussian Mixture Model segmentation for", filepath)
     image = image[:, :, 0]
     show_image((1, 1, 1), "Input Image", image, width=15, height=10, vmin=np.min(image), vmax=np.max(image))
 
     # select initial means from random locations in image
-    means = np.zeros(COMPONENTS)
-    for i in range(COMPONENTS):
+    means = np.zeros(components)
+    for i in range(components):
         rand_col = np.int((cols - 1) * np.random.rand(1))
         rand_row = np.int((rows - 1) * np.random.rand(1))
         means[i] = image[rand_row, rand_col]
-    
-    # initialize other state variables of the algorithm
-    variances = np.float32(np.ones(COMPONENTS)) * INIT_VARIANCE  # initial variance is 1/255 - quite small
-    stdevs = np.sqrt(variances)
-    weights = np.ones(COMPONENTS)
-    logLikelyhood = np.zeros(ITERATIONS)
 
-    for i in range(ITERATIONS):
+    # initialize other state variables of the algorithm
+    variances = np.float64(np.ones(components)) * init_variance  # initial variance is 1/255 - quite small
+    stdevs = np.sqrt(variances)
+    weights = np.ones(components)
+    logLikelyhood = np.zeros(iterations)
+
+    for i in range(iterations):
         # Expectation Step - see page 438 of Pattern Recognition and Machine Learning
-        responsibilities = np.zeros((rows, cols, COMPONENTS))
+        responsibilities = np.zeros((rows, cols, components))
         denominator = np.zeros((rows, cols))  # denominator of responsibilities equation 9.13
-        for k in range(COMPONENTS):
+        for k in range(components):
             denominator = denominator + weights[k] * sp.stats.norm.pdf(image, means[k], stdevs[k])
-        for k in range(COMPONENTS):
-            responsibilities[:, :, k] = np.divide(weights[k] * sp.stats.norm.pdf(image, means[k], stdevs[k]), denominator)  # compute responsibilities eqn 9.13
+        for k in range(components):
+            responsibilities[:, :, k] = np.divide(weights[k] * sp.stats.norm.pdf(image, means[k], stdevs[k]),
+                                                  denominator)  # compute responsibilities eqn 9.13
 
         # Maximization Step - see page 439 of Pattern Recognition and Machine Learning
         numPoints = np.sum(np.sum(responsibilities, axis=0), axis=0)  # compute Nk
-        for k in range(COMPONENTS):
+        for k in range(components):
             means[k] = np.sum(np.sum(np.multiply(responsibilities[:, :, k], image), axis=0), axis=0) / numPoints[k]
-            variances[k] = np.sum(np.sum(np.multiply(responsibilities[:, :, k], image - means[k]), axis=0), axis=0) / numPoints[k]
+            variances[k] = np.sum(np.sum(np.multiply(responsibilities[:, :, k], image - means[k]), axis=0), axis=0) / \
+                           numPoints[k]
             stdevs[k] = np.sqrt(variances[k])
             weights[k] = numPoints[k] / (rows * cols)
         # log likelyhood calculation
@@ -94,12 +103,25 @@ def main():
                 segmentation_output[r, c] = means[segmentation_output_indices[r, c]]
 
         # morphological ops to clean up noise
-        #kernel = np.ones((3, 3))
-        #segmentation_output = cv2.morphologyEx(segmentation_output, cv2.MORPH_OPEN, kernel)
-        show_image((1, 1, 1), "Segmentation Image " + str(i), segmentation_output, width=15, height=10, vmin=np.min(segmentation_output), vmax=np.max(segmentation_output))
+        # kernel = np.ones((3, 3))
+        # segmentation_output = cv2.morphologyEx(segmentation_output, cv2.MORPH_OPEN, kernel)
+        show_image((1, 1, 1), "Segmentation Image " + str(i), segmentation_output, width=15, height=10,
+                   vmin=np.min(segmentation_output), vmax=np.max(segmentation_output))
         print("completed iteration", i, "with log likelyhood", logLikelyhood[i])
     plt.show()
 
+
+def main():
+    if len(sys.argv) != 4:
+        usage()
+        exit()
+    filepath = sys.argv[1]
+    components = int(sys.argv[2])
+    iterations = int(sys.argv[3])
+    if not os.path.exists(filepath):
+        usage()
+        exit()
+    execute_segmentation(filepath, components, iterations)
 
 if __name__ == "__main__":
     main()
