b0c3ca23e5d801e5ccbecee94ed0907bb65d7b34
Alex Hagiopol <alexhagiopol@gmail.com>
2018-09-20 15:57:10 -0700

    modularize E step

--- gmm_segmentation.py
+++ gmm_segmentation.py
@@ -45,6 +45,39 @@ def usage():
     print("Incorrect usage. Example:\npython gmm_segmentation.py image_filepath num_components num_iterations")
 
 
+def compute_likelihoods(intensities, weights_list, means_list, stdevs_list):
+    """
+
+    :param intensities: NxM single layer matrix with 0-1 normalized np.float64 values.
+    :param weights_list: list of np.float64 weight values - one for each of K components
+    :param means_list: list of np.float64 mean values - one for each of K components
+    :param stdevs_list: list of np.float64 stdev values - one for each of K components
+    :return: NxMxK matrix with each layer being a likelihoods matrix
+    """
+    assert(len(weights_list) == len(means_list) == len(stdevs_list))
+    K = len(weights_list)
+    N, M = intensities.shape
+    likelihoods = np.zeros((N, M, K))
+    for k in range(K):
+        likelihoods[:, :, k] = np.multiply(weights_list[k], sp.stats.norm.pdf(intensities, means_list[k], stdevs_list[k]))
+    return likelihoods
+
+
+def compute_responsibilities(likelihoods):
+    """
+    implement equation 9.23 in expectation step
+    :param likelihoods: NxMxK matrix with each layer being a likelihoods matrix
+    """
+    N, M, K = likelihoods.shape
+    responsibilities = np.zeros((N, M, K))
+    likelihoods_sum = np.sum(likelihoods, axis=2)  # compute denominator of responsibilities equation 9.23
+    print("likelIhoods_sum", likelihoods_sum)
+    for k in range(K):
+        responsibilities[:, :, k] = np.divide(likelihoods[:, :, k], likelihoods_sum)
+    print("responsibilities", responsibilities)
+    return responsibilities
+
+
 def execute_segmentation(filepath, components, iterations, init_variance=np.float64(10/255)):
     """
     :param filepath: path to input grayscale image
@@ -53,7 +86,6 @@ def execute_segmentation(filepath, components, iterations, init_variance=np.floa
     :param init_variance: initial variance for each model
     :return:
     """
-
     image = np.divide(np.float64(cv2.imread(filepath)), np.float64(255))  # read image from disk. normalize.
     rows, cols, chans = image.shape
     print("Starting Gaussian Mixture Model segmentation for", filepath)
@@ -77,14 +109,22 @@ def execute_segmentation(filepath, components, iterations, init_variance=np.floa
         print("ITERATION", i)
         print("E STEP")
         # Expectation Step - see page 438 of Pattern Recognition and Machine Learning
+        """
         responsibilities = np.zeros((rows, cols, components))
-        denominator = np.zeros((rows, cols))  # denominator of responsibilities equation 9.13
+
+        component_likelyhoods_sum = np.zeros((rows, cols))
         for k in range(components):
-            denominator = np.add(denominator, np.multiply(weights[k], sp.stats.norm.pdf(image, means[k], stdevs[k])))
+            component_likelyhoods_sum = np.add(component_likelyhoods_sum, np.multiply(weights[k], sp.stats.norm.pdf(image, means[k], stdevs[k])))
+
+        # compute responsibilities = numerator of responsibilities equation 9.23
         for k in range(components):
-            responsibilities[:, :, k] = np.divide(np.multiply(weights[k], sp.stats.norm.pdf(image, means[k], stdevs[k])), denominator)  # compute responsibilities eqn 9.13
-        print("denominator", denominator)
+            responsibilities[:, :, k] = np.divide(np.multiply(weights[k], sp.stats.norm.pdf(image, means[k], stdevs[k])), component_likelyhoods_sum)  # compute responsibilities eqn 9.13
+
+        print("component_likelyhoods_sum", component_likelyhoods_sum)
         print("responsibilities", responsibilities)
+        """
+        responsibilities = compute_responsibilities(compute_likelihoods(image, weights, means, stdevs))
+
         # Maximization Step - see page 439 of Pattern Recognition and Machine Learning
         print("M STEP")
         numPoints = np.sum(np.sum(responsibilities, axis=0), axis=0)  # compute Nk
